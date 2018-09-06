bc84df3f0f717f0c5bb460188cd8e9d0abd052aa
Sharon Miller <sharon@sharonmillercreative.com>
2018-09-06 16:43:56 -0700

    pop complete, test pass

--- test/list.test.js
+++ test/list.test.js
@@ -7,37 +7,45 @@ describe('my list', () => {
     expect(actual.length).toBe(expectedValue);
   });
 
-    test('push should return correct length', () => {
-      let list = new List;
-      let actual = list.push('friends');
-      let expectedValue = list.length;
-      console.log(list);
-      console.log(actual);
-      expect(actual).toEqual(expectedValue);
-    });
-    
-    test('push should return actual list', () => {
-      let list = new List;
-      list.push('hi');
-      list.push('happy');
-      let actual = list;
-      let expectedValue = { 0: 'hi', 1: 'happy', length: 2 };
-      console.log(list);
-      console.log(actual);
-      expect(actual).toEqual(expectedValue);
-    });
+  test('push should return correct length', () => {
+    let list = new List;
+    let actual = list.push('friends');
+    let expectedValue = list.length;
+    console.log(list);
+    console.log(actual);
+    expect(actual).toEqual(expectedValue);
+  });
 
-    test('pop should return actual current list', () => {
-      let list = new List;
-      list.push('hi');
-      list.push('happy');
-      list.pop();
-      let actual = list;
-      let expectedValue = { 0: 'hi', length: 1 };
-      console.log(list);
-      console.log(actual);
-      expect(actual).toEqual(expectedValue);
-    });
+  test('push should return actual list', () => {
+    let list = new List;
+    list.push('hi');
+    list.push('happy');
+    let actual = list;
+    let expectedValue = { 0: 'hi', 1: 'happy', length: 2 };
+    console.log(list);
+    console.log(actual);
+    expect(actual).toEqual(expectedValue);
+  });
 
+  test('pop should return actual current list', () => {
+    let list = new List;
+    list.push('hi');
+    list.push('happy');
+    list.pop();
+    let actual = list;
+    let expectedValue = { 0: 'hi', length: 1 };
+    console.log(list);
+    console.log(actual);
+    expect(actual).toEqual(expectedValue);
+  });
+
+  test('pop should return undefined when no items in array', () => {
+    let list = new List;
+    let actual = list.pop();
+    let expectedValue = undefined;
+    console.log(list);
+    console.log(actual);
+    expect(actual).toEqual(expectedValue);
+  });
 
 });
